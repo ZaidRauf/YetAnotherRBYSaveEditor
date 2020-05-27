2c44edf2f9933560c23de7df53aebef5a74beae1
zrauf <zrauf@sfu.ca>
2020-05-26 18:48:26 -0700

    Added java swing/UI starter code

---
+++ src/UserInterface/StartingWindow.java
@@ -0,0 +1,16 @@
+package UserInterface;
+
+import javax.swing.*;
+
+public class StartingWindow {
+    public static void main(String args[]){
+        JFrame frame = new JFrame("Yet Another Red/Blue/Yellow Save Editor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
+    }
+
+
+}
